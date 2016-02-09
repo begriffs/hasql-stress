d5c54300fdbc6ca3221f1090c78733cadd81b1d3
Joe Nelson <cred+github@begriffs.com>
2016-02-09 15:33:41 -0800

    Read result of update statement

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,12 @@
 module Main where
 
-import Hasql.Connection
+import qualified Hasql.Connection as H
 import qualified Hasql.Session as H
-import Hasql.Query
+import qualified Hasql.Query as H
+import qualified Hasql.Encoders as HE
+import qualified Hasql.Decoders as HD
 
+import Data.Int
 import Data.Pool
 
 import Network.Wai
@@ -12,8 +15,8 @@ import Network.HTTP.Types (status200)
 
 main = do
   let dbUrl = "postgres://postgres@localhost:5432/demo1"
-  pool <- createPool (acquire dbUrl)
-            (either (const $ return ()) release) 1 1 10
+  pool <- createPool (H.acquire dbUrl)
+            (either (const $ return ()) H.release) 1 1 10
 
   let port = 3000
   putStrLn $ "Listening on port " ++ show port
@@ -27,5 +30,14 @@ main = do
   sess = do
     H.sql "begin isolation level read committed;"
     H.sql "set local role 'j';"
-    H.sql "WITH pg_source AS (UPDATE \"public\".\"film\"  SET \"rating\"='1'::unknown  WHERE \"public\".\"film\".\"id\" = '1'::unknown ) SELECT '', 0, '', '';"
+    _ <- H.query () updater
     H.sql "commit;"
+
+updater :: H.Query () Int64
+updater =
+  H.statement sql HE.unit decoder True
+  where
+    sql =
+      "WITH pg_source AS (UPDATE \"public\".\"film\"  SET \"rating\"='1'::unknown  WHERE \"public\".\"film\".\"id\" = '1'::unknown ) SELECT 1;"
+    decoder =
+      HD.singleRow (HD.value HD.int8)
