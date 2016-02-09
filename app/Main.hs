5db84c0e557c13c18a14400e9d51ddbe2fb138ce
Joe Nelson <cred+github@begriffs.com>
2016-02-08 16:22:20 -0800

    Do queries in parallel

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import qualified Control.Concurrent.Thread.Group as TGroup
 import Control.Monad (replicateM_, void)
 import Hasql.Connection
 import Hasql.Session
@@ -10,10 +11,14 @@ import qualified Hasql.Pool as P
 main :: IO ()
 main = do
   let atOnce = 10
+  kids <- TGroup.new
   pool <- P.acquire (atOnce, 0.5, "postgres://postgres@localhost:5432/demo1")
-  replicateM_ atOnce $ P.use pool sess
+  replicateM_ atOnce . TGroup.forkIO kids . void $ P.use pool sess
+
+  TGroup.wait kids
  where
   sess = do
     sql "begin isolation level read committed;"
+    sql "set local role 'j';"
     sql "WITH pg_source AS (UPDATE \"public\".\"film\"  SET \"rating\"='1'::unknown  WHERE \"public\".\"film\".\"id\" = '1'::unknown ) SELECT '', 0, '', '';"
     sql "commit;"
