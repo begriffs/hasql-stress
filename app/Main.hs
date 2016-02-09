173b4719c86392bfdaba57f83dc77d1b73f71409
Joe Nelson <cred+github@begriffs.com>
2016-02-08 18:05:38 -0800

    Add random delays to threads to get more concurrency patterns

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,24 @@
 module Main where
 
 import qualified Control.Concurrent.Thread.Group as TGroup
+import Control.Concurrent (threadDelay)
 import Control.Monad (replicateM_, void)
 import Hasql.Connection
 import Hasql.Session
 import Hasql.Query
 
+import System.Random (getStdRandom, randomR)
+
 import qualified Hasql.Pool as P
 
 main :: IO ()
 main = do
-  let atOnce = 10
+  let atOnce = 3
   kids <- TGroup.new
   pool <- P.acquire (atOnce, 0.5, "postgres://postgres@localhost:5432/demo1")
-  replicateM_ atOnce . TGroup.forkIO kids . void $ P.use pool sess
+  replicateM_ atOnce . TGroup.forkIO kids $ do
+    threadDelay =<< getStdRandom (randomR (0,20000))
+    void $ P.use pool sess
 
   TGroup.wait kids
  where
