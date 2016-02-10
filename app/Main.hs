b6fbea704c5897d9a3b6239185eeea4d84380aca
Joe Nelson <cred+github@begriffs.com>
2016-02-09 17:06:33 -0800

    Create transaction with helper function
    Hoped this would be a problem but it is not

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified Control.Concurrent.Thread.Group as TGroup
 import Control.Concurrent (threadDelay)
 import Control.Monad (replicateM_, void)
+import Data.Monoid ((<>))
 import Hasql.Connection
 import Hasql.Session
 import Hasql.Query
@@ -13,17 +14,28 @@ import qualified Hasql.Pool as P
 
 main :: IO ()
 main = do
-  let atOnce = 3
+  let atOnce = 10
   kids <- TGroup.new
-  pool <- P.acquire (atOnce, 0.5, "postgres://postgres@localhost:5432/demo1")
+  pool <- P.acquire (atOnce, 10, "postgres://postgres@localhost:5432/demo1")
   replicateM_ atOnce . TGroup.forkIO kids $ do
-    threadDelay =<< getStdRandom (randomR (0,20000))
-    void $ P.use pool sess
-
+    threadDelay =<< getStdRandom (randomR (0,1000000))
+    void . P.use pool $ inTransaction ReadCommitted sess
   TGroup.wait kids
+
  where
   sess = do
-    sql "begin isolation level read committed;"
-    sql "set local role 'j';"
     sql "WITH pg_source AS (UPDATE \"public\".\"film\"  SET \"rating\"='1'::unknown  WHERE \"public\".\"film\".\"id\" = '1'::unknown ) SELECT '', 0, '', '';"
-    sql "commit;"
+
+data Isolation = ReadCommitted | RepeatableRead | Serializable
+
+inTransaction :: Isolation -> Session a -> Session a
+inTransaction lvl f = do
+  sql $ "begin " <> isolate <> ";"
+  r <- f
+  sql "commit;"
+  return r
+ where
+  isolate = case lvl of
+    ReadCommitted  -> "ISOLATION LEVEL READ COMMITTED"
+    RepeatableRead -> "ISOLATION LEVEL REPEATABLE READ"
+    Serializable   -> "ISOLATION LEVEL SERIALIZABLE"
