f21620fb81d58593d1fcf6cf9a1cc4a9ef370a2f
Joe Nelson <cred+github@begriffs.com>
2016-02-12 13:45:42 -0800

    Random server delays and a heartbeat

--- app/Main.hs
+++ app/Main.hs
@@ -8,14 +8,25 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.HTTP.Types (status200)
 
+import Data.Time.Clock.POSIX
+import System.Random
+import Control.Concurrent
+import Control.Monad
+
 import qualified Hasql.Pool as P
 
 main = do
-  pool <- P.acquire (10, 0.5, "postgres://postgres@localhost:5432/demo1")
+  void . forkIO . forever $ do
+      t <- round <$> getPOSIXTime :: IO Integer
+      putStrLn $ "alive " ++ show t
+      threadDelay 1000000
+
+  pool <- P.acquire (100, 0.5, "postgres://postgres@localhost:5432/demo1")
 
   let port = 3000
   putStrLn $ "Listening on port " ++ show port
   run port $ \_ respond -> do
+    threadDelay =<< getStdRandom (randomR (0,1000000))
     P.use pool sess
     respond $ responseLBS status200 [] "."
 
